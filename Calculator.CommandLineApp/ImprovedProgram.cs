830b7e2c973d53e3a8fbb2d2589eb785062a5ab2
Jakob Farian Krarup <kr@rup.dk>
2024-09-03 11:00:21 +0200

    Added improved version of Program.cs

---
+++ Calculator.CommandLineApp/ImprovedProgram.cs
@@ -0,0 +1,79 @@
+﻿namespace Calculator.CommandLineApp;
+
+using CalcLib = Calculator.ClassLibrary;
+
+class ImprovedProgram
+{
+    static float firstNumber, secondNumber, result;
+    static string operationToPerform;
+
+    static void ImprovedMain(string[] args)
+    {
+        CheckInputAndExitIfInvalid(args);
+        ParseInput(args[0], args[1], args[2]);
+        CalculateResult();
+        PrintResult();
+    }
+    private static void CheckInputAndExitIfInvalid(string[] args)
+    {
+        if (!ValidateInput(args))
+        {
+            WriteInstructions();
+            
+            // Exit the program with a non-zero exit code to signal an error
+            Environment.Exit(-1);
+        }
+    }
+    private static bool ValidateInput(string[] args)
+    {
+        //NOTE: arguments to a .NET program are also available through "Environment.CommandLine"
+
+        // Check if the input is null or empty
+        if (args == null || args.Length != 3) { return false; }
+        
+        // Check if the operator is one of the valid four options
+        if (args[1] == "+" || args[1] == "-" || args[1] == "*" || args[1] == "/") { return false; }
+        
+        // Check if the first and second numbers are valid floats
+        if (!float.TryParse(args[0], out float firstNumber) || !float.TryParse(args[2], out float secondNumber)) { return false; }
+        
+        return true; //valid input
+    }
+    private static void WriteInstructions()
+    {
+        Console.WriteLine("COMMAND LINE CALCULATOR");
+        Console.WriteLine("Usage: CommandLineCalculator.exe [first number] [operation] [second number]");
+        Console.WriteLine("Example: CommandLineCalculator.exe 4.5 * 3");
+    }
+    private static void ParseInput(string number1, string operation, string number2)
+    {
+        float.TryParse(number1, out firstNumber);
+        float.TryParse(number2, out secondNumber);
+        operationToPerform = operation;
+    }
+    private static void CalculateResult()
+    {
+        float result = 0;
+        switch (operationToPerform)
+        {
+            case "+":
+                result = CalcLib.Calculator.Add(firstNumber, secondNumber);
+                return;
+            case "-":
+                result = CalcLib.Calculator.Subtract(firstNumber, secondNumber);
+                return;
+            case "*":
+                result = CalcLib.Calculator.Multiply(firstNumber, secondNumber);
+                return;
+            case "/":
+                result = CalcLib.Calculator.Divide(firstNumber, secondNumber);
+                return;
+            default:
+                throw new InvalidOperationException($"Unknown operator {operationToPerform}");
+        }
+    }
+    private static void PrintResult()
+    {
+        Console.WriteLine($"{firstNumber} {operationToPerform} {secondNumber} = {result}");
+    }
+}
--- Calculator.CommandLineApp/Program.cs
+++ Calculator.CommandLineApp/Program.cs
@@ -1,54 +1,44 @@
 ﻿using CalcLib = Calculator.ClassLibrary;
-
-namespace Calculator.CommandLineApp
+namespace Calculator.CommandLineApp;
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            InspectArgumentsAndPerformActions(args);
-        }
+        InspectArgumentsAndPerformActions(args);
+    }
 
-        private static void InspectArgumentsAndPerformActions(string[] args)
+    private static void InspectArgumentsAndPerformActions(string[] args)
+    {
+        if (args == null || args.Length == 0)
         {
-            switch (args.Length)
-            {
-                case 0:
-                    WriteInstructions();
-                    break;
-                case 3:
-                    string result = PerformActions(float.Parse(args[0]), args[1], float.Parse(args[2]));
-                    Console.WriteLine(result);
-                    break;
-                default:
-                    Console.WriteLine("Only three arguments are supported");
-                    WriteInstructions();
-                    break;
-            }
+            WriteInstructions();
+            return;
         }
+        string result = PerformActionsAndReturnResult(float.Parse(args[0]), args[1], float.Parse(args[2]));
+        Console.WriteLine(result);
+    }
 
-        private static string PerformActions(float firstNumber, string operationToPerform, float secondNumber)
+    private static string PerformActionsAndReturnResult(float firstNumber, string operationToPerform, float secondNumber)
+    {
+        switch (operationToPerform)
         {
-            switch (operationToPerform)
-            {
-                case "+":
-                    return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Add(firstNumber, secondNumber)}";
-                case "-":
-                    return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Subtract(firstNumber, secondNumber)}";
-                case "*":
-                    return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Multiply(firstNumber, secondNumber)}";
-                case "/":
-                    return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Divide(firstNumber, secondNumber)}";
-                default:
-                    return $"Unknown operator {operationToPerform}";
-            }
+            case "+":
+                return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Add(firstNumber, secondNumber)}";
+            case "-":
+                return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Subtract(firstNumber, secondNumber)}";
+            case "*":
+                return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Multiply(firstNumber, secondNumber)}";
+            case "/":
+                return $"{firstNumber} {operationToPerform} {secondNumber} = {CalcLib.Calculator.Divide(firstNumber, secondNumber)}";
+            default:
+                return $"Unknown operator {operationToPerform}";
         }
+    }
 
-        private static void WriteInstructions()
-        {
-            Console.WriteLine("COMMAND LINE CALCULATOR");
-            Console.WriteLine("Usage: CommandLineCalculator.exe [first number] [operation] [second number]");
-            Console.WriteLine("Example: CommandLineCalculator.exe 4.5 * 3");
-        }
+    private static void WriteInstructions()
+    {
+        Console.WriteLine("COMMAND LINE CALCULATOR");
+        Console.WriteLine("Usage: CommandLineCalculator.exe [first number] [operation] [second number]");
+        Console.WriteLine("Example: CommandLineCalculator.exe 4.5 * 3");
     }
 }
