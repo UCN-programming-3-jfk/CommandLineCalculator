7f439070f30156ae9e404bac51dab0ff1e56b19a
Jakob Farian Krarup <kr@rup.dk>
2026-02-02 16:37:39 +0100

    Updated .NET version from 8.0 to 10.0

--- Calculator.CommandLineApp/Program.cs
+++ Calculator.CommandLineApp/Program.cs
@@ -5,6 +5,7 @@ internal class Program
     static void Main(string[] args)
     {
         InspectArgumentsAndPerformActions(args);
+
     }
 
     private static void InspectArgumentsAndPerformActions(string[] args)
